a0960ea446890683f4841f6af21bc7ae937ceda2
kramarkirill <kkramar@kantiana.ru>
2020-07-23 11:42:18 +0200

    Initial Commit

---
+++ LessonTwo_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LessonTwo_SwiftUI
+//
+//  Created by Кирилл Крамар on 23.07.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
